2b4d7810098a83fc4ba3ede0e9426f2c436da83c
Hidenori Ishii <nori.3dogs@MarinoMacBook-Pro.local>
2023-02-02 10:40:12 +0900

    modify main loop

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import time
 import binascii
 import time
 import led
+import acm1602
 
 GreenLed = led.Led(25,1)
 
 
 
-gpNoOneWire = machine.Pin(4) #GP4
- 
+gpNoOneWire = machine.Pin(28) #GP22
+print('DS18B20 class:  ') 
 ds18b20_sensor = ds18x20.DS18X20(onewire.OneWire(gpNoOneWire))
- 
+print('Acm1602 class:  ') 
+lcddev = acm1602.Acm1602(0,17,16)
+
 sensors = ds18b20_sensor.scan()
  
 print('Found DS18B20: ', sensors)
@@ -22,14 +25,28 @@ print('Found DS18B20: ', sensors)
 while True:
     ds18b20_sensor.convert_temp()
     time.sleep_ms(750)
-    cnt = 0
+    lineflg = 0
+    lcddev.lcdclear()
+    SensNo = 1
     for device in sensors:
-        cnt += 1
         GreenLed.Ledon()
         s = binascii.hexlify(device)
         readable_string = s.decode('ascii')
-        print('DeviceNo:{},SerialNo: {}'.format(cnt,readable_string))
+        if lineflg == 0:
+            print('Sensor 1,SerialNo: {}'.format(readable_string))
+
+            lcddev.lcd1stline()
+            lineflg += 1
+        else:
+            print('Sensor 2,SerialNo: {}'.format(readable_string))
+            lcddev.lcd2ndline()
+            lineflg = 0
         print(ds18b20_sensor.read_temp(device))
+        message = "WT{}:{} C".format(SensNo,ds18b20_sensor.read_temp(device))
+        lcddev.lcdwrite(message)  
         GreenLed.Ledoff()
         time.sleep_ms(300)
+        SensNo += 1
     time.sleep(1)
+
+
